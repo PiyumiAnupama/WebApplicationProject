9122eca80b7017161f2a898f7bd411277845be25
PiyumiAnupama <96447@fhss.sjp.ac.lk>
2022-07-10 14:50:28 +0530

    connected to mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
+mongoose.connect("mongodb+srv://Anupama:Anupama@cluster0.3cwbh.mongodb.net/webApp?retryWrites=true&w=majority")
+.then(() => app.listen(5000))
+.then(() =>
+console.log("Connected TO Database and Listening TO Localhost 5000")
+)
+.catch((err) => console.log(err));
 
-app.use("/api", (req,res,next)=>{
-    res.send("hello world");
-});
-app.listen(5000);
+
+
 
